85468f799c062664a67f902bbb8da5c72f39b52d
myworldbox <54nicholasleung45@gmail.com>
2020-02-14 03:08:56 +0800

    .
    .

--- MAD/MadBox/app/src/main/java/com/example/madbox/MainActivity.java
+++ MAD/MadBox/app/src/main/java/com/example/madbox/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.madbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    int splashTime = 5000; //Splash screen time
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +26,51 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        //Start timer and take action after the timer ends
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String text = "";
+
+        switch (view.getId()) {
+
+            case R.id.button1:
 
                 //Move to next page
-                Intent mySuperIntent = new Intent(MainActivity.this, HomeActivity.class);
+                text = "Intro";
+                Intent mySuperIntent = new Intent(MainActivity.this, Website1Activity.class);
                 startActivity(mySuperIntent);
-                finish();
 
-            }
-        }, splashTime);
+                break;
+            case R.id.button2:
+                text = "Game1";
+                break;
+            case R.id.button3:
+                text = "Game2";
+                break;
+            case R.id.button4:
+                text = "Game3";
+                break;
+            case R.id.button5:
+                text = "Setting";
+                break;
+
+        }
+
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
     }
 }
