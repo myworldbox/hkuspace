0c32640878fa14cf4038d5388d978e8f52b37d93
myworldbox <54nicholasleung45@gmail.com>
2020-03-31 15:03:29 +0800

    .

--- ICP/Number.c
+++ ICP/Number.c
@@ -1,28 +1,37 @@
+//
+//  main.c
+//  lab10_1.c
+//
+//  Created by Nicholas Leung on 27/11/2018.
+//  Copyright © 2018 Nicholas Leung. All rights reserved.
+//
+
 #include <stdio.h>
- 
-int secs(int,int,int);
-int secsRef(int,int,int,int);
-void main() {
-    
-    int = a,b,c;
- 
- 
-    printf("\nEnter a number of hours:");
-    gets(a);
-    printf("\nEnter a number of minutes:");
-    gets(b);
-     printf("\nEnter a number of secords:");
-    gets(c);
-    
-    secs(a,b,c);
-    secsRef(a,b,c,&totalsec);
-}
+
 int secs(int a, int b, int c){
-	int totalsec=a*60*60+b*60+c;
-	return totalsec;
+    
+    int totalsec=a*60*60+b*60+c;
+    printf("\nThe total number of seconds from function [secs] is %d",totalsec);
+    return totalsec;
+    
 }
 
-int secsRef(int a, int b, int c, int *totsec){
-	int totalsec= &totalsec;
-	
+void secsRef(int *totalsec){
+    
+    printf("\n\nThe total number of seconds from function [secs] is %d",*totalsec);
+    
+}
+
+void main() {
+    
+    int a,b,c;
+    printf("Enter a number of hours: ");
+    scanf("%d",&a);
+    printf("Enter a number of minutes: ");
+    scanf("%d",&b);
+    printf("Enter a number of secords: ");
+    scanf("%d",&c);
+    int totalsec = secs(a,b,c);
+    secsRef(&totalsec);
+    
 }
